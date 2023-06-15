1ddde61449131b5b6d1af68b8ae1d529d4528a56
Artyom Blokhin <akrovart@gmail.com>
2023-06-15 12:49:54 +0300

    Форма регистрации

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.hashers import check_password
 
 User = get_user_model()
 
+
 class UserLoginForm(forms.Form):
     email = forms.CharField(widget=forms.EmailInput(attrs={
         'class': 'form-control'
@@ -12,15 +13,15 @@ class UserLoginForm(forms.Form):
         'class': 'form-control'
     }))
 
-    def clean(self, *args, **kwargs): # Валидация
+    def clean(self, *args, **kwargs):  # Валидация
         email = self.cleaned_data.get('email').strip()
-        password = self.cleaned_data.get('password')
+        password = self.cleaned_data.get('password').strip()
 
         if email and password:
             qs = User.objects.filter(email=email)
             if not qs.exists:
                 raise forms.ValidationError("Пользователь не найден!")
-            if not check_password(password, qs[0].password): # равен ли пароль полученный в фильтре
+            if not check_password(password, qs[0].password):  # равен ли пароль полученный в фильтре
                 raise forms.ValidationError("Пароль неверный!")
 
             user = authenticate(email=email, password=password)
@@ -30,5 +31,27 @@ class UserLoginForm(forms.Form):
             return super(UserLoginForm, self).clean(*args, **kwargs)
 
 
+class UserRegistrationForm(forms.ModelForm):
+    email = forms.CharField(label='Введите email', widget=forms.EmailInput(attrs={
+        'class': 'form-control'
+    }))
+    password = forms.CharField(label='Введите пароль',
+                               widget=forms.PasswordInput(attrs={
+        'class': 'form-control'
+    }))
+    password2 = forms.CharField(label='Введите пароль еще раз',
+                                widget=forms.PasswordInput(attrs={
+        'class': 'form-control'
+    }))
 
+    class Meta:
+        model = User
+        fields = ('email', )
+
+    def clean_password2(self): # он будет вызван, потому что все методы, которые начинаются на clean будут вызваны автоматически
+        data = self.cleaned_data
+        if data['password'] != data['password2']:
+            raise forms.ValidationError('Пароли не совпадают!')
+
+        return data['password2']
 
