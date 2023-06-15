61c79cf2481dd3f39f53eb0fcdffd5cd824ec1c8
Artyom Blokhin <akrovart@gmail.com>
2023-06-15 09:58:41 +0300

    Добавлен асинхронный режим

--- run_scraping.py
+++ run_scraping.py
@@ -1,52 +1,75 @@
 import codecs
 import os, sys
 
-proj = os.path.dirname(os.path.abspath('manage.py')) # Найден путь до проекта
+from django.contrib.auth import get_user_model
+from django.db import DatabaseError
+
+proj = os.path.dirname(os.path.abspath('manage.py'))
 sys.path.append(proj)
-os.environ['DJANGO_SETTINGS_MODULE'] = "scraping_service.settings"
+os.environ["DJANGO_SETTINGS_MODULE"] = "scraping_service.settings"
 
 import django
-django.setup() # Для запуска django
+django.setup()
 
-from django.db import DatabaseError
-from django.contrib.auth import get_user_model
 from scraping.parsers import *
-from scraping.models import Vacancy, City, Language, Error
+from scraping.models import Vacancy, City, Language, Error, Url
 
-User = get_user_model() # Получение пользователя
+User = get_user_model()
 
 parsers = (
-    (work, 'https://www.work.ua/ru/jobs-kyiv-python/'),
-    (dou, 'https://jobs.dou.ua/vacancies/?city=%D0%9A%D0%B8%D0%B5%D0%B2&category=Python'),
-    (djinni, 'https://djinni.co/jobs/?location=%D0%9A%D0%B8%D0%B5%D0%B2&primary_keyword=Python'),
-    (rabota, 'https://rabota.ua/zapros/python/%d0%ba%d0%b8%d0%b5%d0%b2')
+    (work, 'work'),
+    (djinni, 'djinni'),
+    (rabota, 'rabota')
 )
+
+jobs, errors = [], []
 
 def get_settings():
     qs = User.objects.filter(send_email=True).values()
     settings_lst = set((q['city_id'], q['language_id']) for q in qs)
     return settings_lst
 
-q = get_settings()
-city = City.objects.filter(slug='kiev').first()
-language = Language.objects.filter(slug='python').first()
 
-jobs, errors = [], []
-for func, url in parsers:
-    j, e = func(url)
-    jobs += j
-    errors += e
+def get_urls(_settings):
+    qs = Url.objects.all().values()
+    url_dct = {(q['city_id'], q['language_id']): q['url_data'] for q in qs}
+    urls = []
+    for pair in _settings:
+        tmp = {}
+        tmp['city'] = pair[0]
+        tmp['language'] = pair[1]
+        tmp['url_data'] = url_dct[pair]
+        urls.append(tmp)
+    return urls
+
+async def main(value):
+    func, url, city, language = value
+    job, error = await loop.run_in_executor(None, func, url, city, language)
+    errors.extend(error)
+    jobs.extend(job)
+
+settings = get_settings()
+url_list = get_urls(settings)
+
+import asyncio
+
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+
+tmp_tasks = [
+    (func, data['url_data'][key], data['city'], data['language']) for data in url_list
+    for func, key in parsers
+]
+tasks = asyncio.wait([loop.create_task(main(f)) for f in tmp_tasks])
+
+loop.run_until_complete(tasks)
+loop.close()
 
 for job in jobs:
-    v = Vacancy(**job, city=city, language=language)
+    v = Vacancy(**job)
     try:
         v.save()
     except DatabaseError:
         pass
-
 if errors:
     er = Error(data=errors).save()
-
-h = codecs.open('work.txt', 'w', 'utf-8')
-h.write(str(jobs))
-h.close()
